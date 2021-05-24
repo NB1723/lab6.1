bd69f3c7b044766315b11d046555e7dff90a6d92
NB1723 <notalia03@gmail.com>
2021-05-24 07:02:50 +0300

    lab6.1

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+#include "../Array.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			TArray a = new Array::value_type[4];
+			a[0] = 1;
+			a[1] = 2;
+			a[2] = 3;
+			a[3] = 4;
+			Array result = Calc(4, a);
+			Assert::AreEqual(result[0], 10.);
+			Assert::AreEqual(result[1], 2.5);
+		}
+	};
+}
